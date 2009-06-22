d7b8ec454713fb82efdbc69ab79ae6143bd0364f
George Ogata <george.ogata@gmail.com>
2009-06-21 22:29:15 -0400

    Add autospec support.

--- spec/rspec_matchers_spec.rb
+++ spec/rspec_matchers_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'spec/spec_helper'
 require 'command_rat/rspec/matchers'
 
 describe "an RSpec context" do
--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'spec/spec_helper'
 require 'fileutils'
 
 describe "CommandRat::Session" do
